7bed7b075f72429e5506dcfe309cb00436f40216
sundegan <2226523482@qq.com>
2023-11-28 22:13:48 +0800

    add comments and test code for pytorch_i3d.py

--- pytorch_i3d.py
+++ pytorch_i3d.py
@@ -1,51 +1,61 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
 
 import numpy as np
 
-import os
-import sys
-from collections import OrderedDict
-
 
 class MaxPool3dSamePadding(nn.MaxPool3d):
-    
+    """
+    MaxPool3dSamePadding类是对PyTorch中的MaxPool3d的扩展，添加了"same"的填充功能。
+    在进行最大池化操作时，会自动计算并应用必要的填充，以确保输出特征图的尺寸和输入特征图的尺寸在空间维度上尽可能相似。
+    """
+
     def compute_pad(self, dim, s):
+        """
+        compute_pad()用于计算在特定维度上需要的填充量，根据池化核大小、步长和输入特征图的尺寸计算。
+        Args:
+            dim: 指定计算填充量的维度。在三维情况下，dim可以是0、1、2，分别对应于时间维度(深度)、高度和宽度。
+            s: 输入特征图在指定维度上的大小。比如，如果dim为1(高度)，s就是输入特征图的高度。
+
+        Returns: 函数返回计算得到的填充量，表示在指定维度上总共需要添加的填充数量。
+        """
         if s % self.stride[dim] == 0:
             return max(self.kernel_size[dim] - self.stride[dim], 0)
         else:
             return max(self.kernel_size[dim] - (s % self.stride[dim]), 0)
 
     def forward(self, x):
-        # compute 'same' padding
+        # 获取输入x的尺寸
         (batch, channel, t, h, w) = x.size()
-        #print t,h,w
+        # 计算输出尺寸
         out_t = np.ceil(float(t) / float(self.stride[0]))
         out_h = np.ceil(float(h) / float(self.stride[1]))
         out_w = np.ceil(float(w) / float(self.stride[2]))
-        #print out_t, out_h, out_w
+        # 计算各个维度的填充量
         pad_t = self.compute_pad(0, t)
         pad_h = self.compute_pad(1, h)
         pad_w = self.compute_pad(2, w)
-        #print pad_t, pad_h, pad_w
-
+        # 将每个维度的填充量均匀分配到前后（或上下、左右）两侧
         pad_t_f = pad_t // 2
         pad_t_b = pad_t - pad_t_f
         pad_h_f = pad_h // 2
         pad_h_b = pad_h - pad_h_f
         pad_w_f = pad_w // 2
         pad_w_b = pad_w - pad_w_f
-
+        # 使用F.pad()对输入x应用计算的填充量后进行最大池化
         pad = (pad_w_f, pad_w_b, pad_h_f, pad_h_b, pad_t_f, pad_t_b)
-        #print x.size()
-        #print pad
+        # print x.size()
+        # print pad
         x = F.pad(x, pad)
-        return super(MaxPool3dSamePadding, self).forward(x)
-    
+        return super().forward(x)
+
 
 class Unit3D(nn.Module):
+    """
+    Unit3D是一个基本的3D卷积单元，是构建I3D模型的基础模块，包含基本的3D卷积层、可选的批量归一化层和激活函数层。
+    通过组合3D卷积、批量归一化和激活函数，能够提取数据中的时空特征，同时通过动态填充机制，还能够在执行卷积操作时保持特征图的空间尺寸。
+    """
 
     def __init__(self, in_channels,
                  output_channels,
@@ -56,10 +66,21 @@ class Unit3D(nn.Module):
                  use_batch_norm=True,
                  use_bias=False,
                  name='unit_3d'):
-        
-        """Initializes Unit3D module."""
-        super(Unit3D, self).__init__()
-        
+        """
+        初始化Unit3D模块。
+        Args:
+            in_channels: 输入通道数。
+            output_channels: 输出通道数。
+            kernel_shape: 卷积核形状，是一个三元组，分别代表深度、高度和宽度上的卷积核尺寸。
+            stride: 卷积的步长。
+            padding: 卷积的填充量，默认值为0表示不填充。
+            activation_fn: 激活函数，默认为ReLU函数。
+            use_batch_norm: 是否使用批量归一化。
+            use_bias: 表示卷积层是否使用偏置项。
+            name: 模块的名称。
+        """
+        super().__init__()
+
         self._output_channels = output_channels
         self._kernel_shape = kernel_shape
         self._stride = stride
@@ -68,14 +89,16 @@ class Unit3D(nn.Module):
         self._use_bias = use_bias
         self.name = name
         self.padding = padding
-        
+
+        # 定义3D卷积层
         self.conv3d = nn.Conv3d(in_channels=in_channels,
                                 out_channels=self._output_channels,
                                 kernel_size=self._kernel_shape,
                                 stride=self._stride,
-                                padding=0, # we always want padding to be 0 here. We will dynamically pad based on input size in forward function
+                                padding=0,  # 这里总是设为0，通过在forward中动态地进行填充而不是静态地设置一个固定的填充值
                                 bias=self._use_bias)
-        
+
+        # 定义批量归一化层（可选地）
         if self._use_batch_norm:
             self.bn = nn.BatchNorm3d(self._output_channels, eps=0.001, momentum=0.01)
 
@@ -85,19 +108,18 @@ class Unit3D(nn.Module):
         else:
             return max(self._kernel_shape[dim] - (s % self._stride[dim]), 0)
 
-            
     def forward(self, x):
         # compute 'same' padding
         (batch, channel, t, h, w) = x.size()
-        #print t,h,w
+        # print t,h,w
         out_t = np.ceil(float(t) / float(self._stride[0]))
         out_h = np.ceil(float(h) / float(self._stride[1]))
         out_w = np.ceil(float(w) / float(self._stride[2]))
-        #print out_t, out_h, out_w
+        # print out_t, out_h, out_w
         pad_t = self.compute_pad(0, t)
         pad_h = self.compute_pad(1, h)
         pad_w = self.compute_pad(2, w)
-        #print pad_t, pad_h, pad_w
+        # print pad_t, pad_h, pad_w
 
         pad_t_f = pad_t // 2
         pad_t_b = pad_t - pad_t_f
@@ -107,46 +129,68 @@ class Unit3D(nn.Module):
         pad_w_b = pad_w - pad_w_f
 
         pad = (pad_w_f, pad_w_b, pad_h_f, pad_h_b, pad_t_f, pad_t_b)
-        #print x.size()
-        #print pad
+        # print x.size()
+        # print pad
         x = F.pad(x, pad)
-        #print x.size()        
+        # print x.size()
 
+        # 进行填充后进行三维卷积
         x = self.conv3d(x)
+        # 进行批量归一化(可选地)
         if self._use_batch_norm:
             x = self.bn(x)
+        # 如果指定了激活函数，则应用激活函数(默认为ReLU)
         if self._activation_fn is not None:
             x = self._activation_fn(x)
         return x
 
 
-
 class InceptionModule(nn.Module):
-    def __init__(self, in_channels, out_channels, name):
-        super(InceptionModule, self).__init__()
+    """
+    InceptionModule是一个实现Inception架构的基本模块。
+    Inception架构最初在GoogleNet(Inception v1)中提出，其核心思想是在同一层内并行使用不同尺寸的卷积核，从而能够在单个层级上捕捉不同尺度的空间特征。
+    papers：https://arxiv.org/abs/1409.4842
+    """
 
-        self.b0 = Unit3D(in_channels=in_channels, output_channels=out_channels[0], kernel_shape=[1, 1, 1], padding=0,
-                         name=name+'/Branch_0/Conv3d_0a_1x1')
-        self.b1a = Unit3D(in_channels=in_channels, output_channels=out_channels[1], kernel_shape=[1, 1, 1], padding=0,
-                          name=name+'/Branch_1/Conv3d_0a_1x1')
-        self.b1b = Unit3D(in_channels=out_channels[1], output_channels=out_channels[2], kernel_shape=[3, 3, 3],
-                          name=name+'/Branch_1/Conv3d_0b_3x3')
-        self.b2a = Unit3D(in_channels=in_channels, output_channels=out_channels[3], kernel_shape=[1, 1, 1], padding=0,
-                          name=name+'/Branch_2/Conv3d_0a_1x1')
-        self.b2b = Unit3D(in_channels=out_channels[3], output_channels=out_channels[4], kernel_shape=[3, 3, 3],
-                          name=name+'/Branch_2/Conv3d_0b_3x3')
-        self.b3a = MaxPool3dSamePadding(kernel_size=[3, 3, 3],
-                                stride=(1, 1, 1), padding=0)
-        self.b3b = Unit3D(in_channels=in_channels, output_channels=out_channels[5], kernel_shape=[1, 1, 1], padding=0,
-                          name=name+'/Branch_3/Conv3d_0b_1x1')
+    def __init__(self, in_channels, out_channels, name):
+        """
+        在该构造函数中，定义了四个分支：Branch_0、Branch_1、Branch_2、Branch_3，
+        分别使用不同大小的卷积核同时捕获输入数据在不同尺度上的特征，每个分支输出的特征图数量由out_channels数组指定。
+        b0：一个1*1*1的卷积核，主要用于捕捉局部的、细粒度的特征。
+        b1a和b1b：先是一个1*1*1的卷积核减少通道数（有助于降低计算量），然后b1b使用3x3x3的卷积核捕获中等尺度的特征。
+        b2a和b2b：类似于b1，但可能使用不同数量的通道或不同尺寸的卷积核。b2a用于降维，而b2b用于捕捉更大尺度的特征。
+        b3a和b3b：先是一个3*3*3的最大池化操作可以捕获更广泛的特征，随后使用1x1x1的卷积核进行降维。
+        Args:
+            in_channels: 输入通道数。
+            out_channels: 输出通道数。
+            name: 模块名称。
+        """
+        super().__init__()
+
+        self.b0 = Unit3D(in_channels=in_channels, output_channels=out_channels[0], kernel_shape=(1, 1, 1), padding=0,
+                         name=name + '/Branch_0/Conv3d_0a_1x1')
+        self.b1a = Unit3D(in_channels=in_channels, output_channels=out_channels[1], kernel_shape=(1, 1, 1), padding=0,
+                          name=name + '/Branch_1/Conv3d_0a_1x1')
+        self.b1b = Unit3D(in_channels=out_channels[1], output_channels=out_channels[2], kernel_shape=(3, 3, 3),
+                          name=name + '/Branch_1/Conv3d_0b_3x3')
+        self.b2a = Unit3D(in_channels=in_channels, output_channels=out_channels[3], kernel_shape=(1, 1, 1), padding=0,
+                          name=name + '/Branch_2/Conv3d_0a_1x1')
+        self.b2b = Unit3D(in_channels=out_channels[3], output_channels=out_channels[4], kernel_shape=(3, 3, 3),
+                          name=name + '/Branch_2/Conv3d_0b_3x3')
+        self.b3a = MaxPool3dSamePadding(kernel_size=(3, 3, 3),
+                                        stride=(1, 1, 1), padding=0)
+        self.b3b = Unit3D(in_channels=in_channels, output_channels=out_channels[5], kernel_shape=(1, 1, 1), padding=0,
+                          name=name + '/Branch_3/Conv3d_0b_1x1')
         self.name = name
 
-    def forward(self, x):    
+    def forward(self, x):
+        # 输入数据x依次通过四个分支，然后将四个分支的输出在通道维度(dim=1)上拼接起来，
+        # 拼接后的总输出通道数=out_channels[0]+out_channels[2]+out_channels[4]+out_channels[5]。
         b0 = self.b0(x)
         b1 = self.b1b(self.b1a(x))
         b2 = self.b2b(self.b2a(x))
         b3 = self.b3b(self.b3a(x))
-        return torch.cat([b0,b1,b2,b3], dim=1)
+        return torch.cat([b0, b1, b2, b3], dim=1)
 
 
 class InceptionI3d(nn.Module):
@@ -220,80 +264,83 @@ class InceptionI3d(nn.Module):
         self.end_points = {}
         end_point = 'Conv3d_1a_7x7'
         self.end_points[end_point] = Unit3D(in_channels=in_channels, output_channels=64, kernel_shape=[7, 7, 7],
-                                            stride=(2, 2, 2), padding=(3,3,3),  name=name+end_point)
+                                            stride=(2, 2, 2), padding=(3, 3, 3), name=name + end_point)
         if self._final_endpoint == end_point: return
-        
+
         end_point = 'MaxPool3d_2a_3x3'
         self.end_points[end_point] = MaxPool3dSamePadding(kernel_size=[1, 3, 3], stride=(1, 2, 2),
-                                                             padding=0)
+                                                          padding=0)
         if self._final_endpoint == end_point: return
-        
+
         end_point = 'Conv3d_2b_1x1'
         self.end_points[end_point] = Unit3D(in_channels=64, output_channels=64, kernel_shape=[1, 1, 1], padding=0,
-                                       name=name+end_point)
+                                            name=name + end_point)
         if self._final_endpoint == end_point: return
-        
+
         end_point = 'Conv3d_2c_3x3'
         self.end_points[end_point] = Unit3D(in_channels=64, output_channels=192, kernel_shape=[3, 3, 3], padding=1,
-                                       name=name+end_point)
+                                            name=name + end_point)
         if self._final_endpoint == end_point: return
 
         end_point = 'MaxPool3d_3a_3x3'
         self.end_points[end_point] = MaxPool3dSamePadding(kernel_size=[1, 3, 3], stride=(1, 2, 2),
-                                                             padding=0)
+                                                          padding=0)
         if self._final_endpoint == end_point: return
-        
+
         end_point = 'Mixed_3b'
-        self.end_points[end_point] = InceptionModule(192, [64,96,128,16,32,32], name+end_point)
+        self.end_points[end_point] = InceptionModule(192, [64, 96, 128, 16, 32, 32], name + end_point)
         if self._final_endpoint == end_point: return
 
         end_point = 'Mixed_3c'
-        self.end_points[end_point] = InceptionModule(256, [128,128,192,32,96,64], name+end_point)
+        self.end_points[end_point] = InceptionModule(256, [128, 128, 192, 32, 96, 64], name + end_point)
         if self._final_endpoint == end_point: return
 
         end_point = 'MaxPool3d_4a_3x3'
         self.end_points[end_point] = MaxPool3dSamePadding(kernel_size=[3, 3, 3], stride=(2, 2, 2),
-                                                             padding=0)
+                                                          padding=0)
         if self._final_endpoint == end_point: return
 
         end_point = 'Mixed_4b'
-        self.end_points[end_point] = InceptionModule(128+192+96+64, [192,96,208,16,48,64], name+end_point)
+        self.end_points[end_point] = InceptionModule(128 + 192 + 96 + 64, [192, 96, 208, 16, 48, 64], name + end_point)
         if self._final_endpoint == end_point: return
 
         end_point = 'Mixed_4c'
-        self.end_points[end_point] = InceptionModule(192+208+48+64, [160,112,224,24,64,64], name+end_point)
+        self.end_points[end_point] = InceptionModule(192 + 208 + 48 + 64, [160, 112, 224, 24, 64, 64], name + end_point)
         if self._final_endpoint == end_point: return
 
         end_point = 'Mixed_4d'
-        self.end_points[end_point] = InceptionModule(160+224+64+64, [128,128,256,24,64,64], name+end_point)
+        self.end_points[end_point] = InceptionModule(160 + 224 + 64 + 64, [128, 128, 256, 24, 64, 64], name + end_point)
         if self._final_endpoint == end_point: return
 
         end_point = 'Mixed_4e'
-        self.end_points[end_point] = InceptionModule(128+256+64+64, [112,144,288,32,64,64], name+end_point)
+        self.end_points[end_point] = InceptionModule(128 + 256 + 64 + 64, [112, 144, 288, 32, 64, 64], name + end_point)
         if self._final_endpoint == end_point: return
 
         end_point = 'Mixed_4f'
-        self.end_points[end_point] = InceptionModule(112+288+64+64, [256,160,320,32,128,128], name+end_point)
+        self.end_points[end_point] = InceptionModule(112 + 288 + 64 + 64, [256, 160, 320, 32, 128, 128],
+                                                     name + end_point)
         if self._final_endpoint == end_point: return
 
         end_point = 'MaxPool3d_5a_2x2'
         self.end_points[end_point] = MaxPool3dSamePadding(kernel_size=[2, 2, 2], stride=(2, 2, 2),
-                                                             padding=0)
+                                                          padding=0)
         if self._final_endpoint == end_point: return
 
         end_point = 'Mixed_5b'
-        self.end_points[end_point] = InceptionModule(256+320+128+128, [256,160,320,32,128,128], name+end_point)
+        self.end_points[end_point] = InceptionModule(256 + 320 + 128 + 128, [256, 160, 320, 32, 128, 128],
+                                                     name + end_point)
         if self._final_endpoint == end_point: return
 
         end_point = 'Mixed_5c'
-        self.end_points[end_point] = InceptionModule(256+320+128+128, [384,192,384,48,128,128], name+end_point)
+        self.end_points[end_point] = InceptionModule(256 + 320 + 128 + 128, [384, 192, 384, 48, 128, 128],
+                                                     name + end_point)
         if self._final_endpoint == end_point: return
 
         end_point = 'Logits'
         self.avg_pool = nn.AvgPool3d(kernel_size=[2, 7, 7],
                                      stride=(1, 1, 1))
         self.dropout = nn.Dropout(dropout_keep_prob)
-        self.logits = Unit3D(in_channels=384+384+128+128, output_channels=self._num_classes,
+        self.logits = Unit3D(in_channels=384 + 384 + 128 + 128, output_channels=self._num_classes,
                              kernel_shape=[1, 1, 1],
                              padding=0,
                              activation_fn=None,
@@ -303,36 +350,41 @@ class InceptionI3d(nn.Module):
 
         self.build()
 
-
     def replace_logits(self, num_classes):
         self._num_classes = num_classes
-        self.logits = Unit3D(in_channels=384+384+128+128, output_channels=self._num_classes,
+        self.logits = Unit3D(in_channels=384 + 384 + 128 + 128, output_channels=self._num_classes,
                              kernel_shape=[1, 1, 1],
                              padding=0,
                              activation_fn=None,
                              use_batch_norm=False,
                              use_bias=True,
                              name='logits')
-        
-    
+
     def build(self):
         for k in self.end_points.keys():
             self.add_module(k, self.end_points[k])
-        
+
     def forward(self, x):
         for end_point in self.VALID_ENDPOINTS:
             if end_point in self.end_points:
-                x = self._modules[end_point](x) # use _modules to work with dataparallel
+                x = self._modules[end_point](x)  # use _modules to work with dataparallel
 
         x = self.logits(self.dropout(self.avg_pool(x)))
         if self._spatial_squeeze:
             logits = x.squeeze(3).squeeze(3)
         # logits is batch X time X classes, which is what we want to work with
         return logits
-        
 
     def extract_features(self, x):
         for end_point in self.VALID_ENDPOINTS:
             if end_point in self.end_points:
                 x = self._modules[end_point](x)
         return self.avg_pool(x)
+
+
+if __name__ == '__main__':
+    # 测试InceptionModule模块和预期是否相符
+    inception = InceptionModule(3, [1, 2, 3, 4, 5, 6], 'test')
+    x = torch.rand(1, 3, 1, 64, 64)  # batch=1, channels=3, t=1, h=64, w=64
+    out = inception(x)
+    print(out.shape)  # torch.Size([1, 15, 1, 64, 64]), 1+3+5+6=15
